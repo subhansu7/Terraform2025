4e2cd8d2cb5c7cd824636fa7a3fd51c1ec92351c
subhansu7 <sarangisubhansu7@gmail.com>
2025-02-14 17:58:33 +0530

    adding chnages in main.tf for additional resource parameters to track behavior of statefile

--- Day-3-Statefile/main.tf
+++ Day-3-Statefile/main.tf
@@ -1,5 +1,8 @@
 resource "aws_instance" "name" {
     ami = "ami-085ad6ae776d8f09c"
+    availability_zone = "us-east-1b"
     instance_type = "t2.nano"
     key_name = "key-n-virginia"
+    tags = {name = "dev-instance"}
+    security_groups =  ["test-sg"]
 }
