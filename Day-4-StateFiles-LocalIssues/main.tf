f33b5682110462cc1890831bdfd05592e75f05ff
subhansu7 <sarangisubhansu7@gmail.com>
2025-02-16 21:04:06 +0530

    statefile

---
+++ Day-4-StateFiles-LocalIssues/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "name" {
+    ami = "ami-085ad6ae776d8f09c"
+    availability_zone = "us-east-1b"
+    instance_type = "t2.nano"
+    key_name = "key-n-virginia"
+    tags = {
+        Name = "dev"
+        }  
+}
