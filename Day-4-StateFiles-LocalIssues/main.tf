d05208810779eb46020f12ef9b70f80c54ae445e
subhansu7 <sarangisubhansu7@gmail.com>
2025-02-16 21:32:56 +0530

    updated notes in main.tf

--- Day-4-StateFiles-LocalIssues/main.tf
+++ Day-4-StateFiles-LocalIssues/main.tf
@@ -1,3 +1,9 @@
+#note Developers Overwriting Each Other’s Work if separe state file for same project
+#Merge Conflicts in terraform.tfstate
+#If two developers run terraform apply at the same time, one may overwrite the other's changes, causing unexpected resource modifications.
+#both developers working independetly not collabrative 
+#solution: maintain common ststefile to overocme above issues 
+
 resource "aws_instance" "name" {
     ami = "ami-085ad6ae776d8f09c"
     availability_zone = "us-east-1b"
