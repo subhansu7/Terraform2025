3b68b3dced6aa869d7a89274970005e7efc027cc
subhansu7 <sarangisubhansu7@gmail.com>
2025-02-17 15:59:08 +0530

    adding functionality to create multiple satefile in s3 bucket

---
+++ Day-5-Display-Output/backend.tf
@@ -0,0 +1,12 @@
+#creating s3 bucket and dynamo DB for state backend storgae and applying the lock mechanisam for statefile
+
+# This backend configuration instructs Terraform to store its state in an S3 bucket.
+terraform {
+    backend "s3" {
+        bucket         = "my-unique-779-s3-bucket"  # Name of the S3 bucket where the state will be stored.
+        region         =  "us-east-1"
+        key            = "day-5/terraform.tfstate" # Path within the bucket where the state will be read/written.
+        dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, note: first run day-4-s3-dynamodb resource then day-4-state-remote-backend
+        encrypt        = true  # Ensures the state is encrypted at rest in S3.
+    }
+}
