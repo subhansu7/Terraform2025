c0b7510d119d8d1e2e96fa7a5292c1af0f5cff8b
subhansu7 <sarangisubhansu7@gmail.com>
2025-02-18 09:05:45 +0530

    Few variable change in custoom AWS Network

--- Day-5-AWS-NetworkComponents/main.tf
+++ Day-5-AWS-NetworkComponents/main.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "prod" {
         name = "prod_vpc"
     }
 }
-#create subnet
+#create public subnet
 resource "aws_subnet" "prod" {
     vpc_id = aws_vpc.prod.id
     cidr_block = "10.0.0.0/24"
@@ -21,19 +21,57 @@ resource "aws_internet_gateway" "prod" {
     }
 }
 #Create Route Table and edit routes
-resource "aws_route_table" "prod" {
+resource "aws_route_table" "pub_rt" {
     vpc_id = aws_vpc.prod.id
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.prod.id
     }
 }
-#Assocaite subnet with route table
-resource "aws_route_table_association" "name" {
+#Assocaite public subnet with route table
+resource "aws_route_table_association" "public_subnet_assoc" {
     subnet_id = aws_subnet.prod.id
-    route_table_id = aws_route_table.prod.id  
+    route_table_id = aws_route_table.pub_rt.id  
 }
 
+#Create private subnet
+resource "aws_subnet" "prod_private" {
+    vpc_id = aws_vpc.prod.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+      Name = "pvt_subnet"
+    }  
+}
+
+#Create elastic IP
+resource "aws_eip" "nat_eip" {
+  tags = {
+    Name = "nat_eip"
+  }
+}
+
+#Create NAT Gateway
+resource "aws_nat_gateway" "prod" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = aws_subnet.prod.id
+    tags = {
+        name = "prod_nat"
+    }
+}
+
+#Create Route Table for NAT Gateway and edit routes
+resource "aws_route_table" "pvt_rt" {
+    vpc_id = aws_vpc.prod.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.prod.id
+    }
+}
+#Assocaite private subnet with route table
+resource "aws_route_table_association" "private_subnet_assoc" {
+    subnet_id = aws_subnet.prod_private.id
+    route_table_id = aws_route_table.pvt_rt.id 
+}
 #create security group
 resource "aws_security_group" "prod" {
     name = "prod_sg"
@@ -58,9 +96,8 @@ resource "aws_security_group" "prod" {
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/24"]
     }
-
 }
-#Create instance
+#Create public instance
 resource "aws_instance" "name" {
     ami = "ami-085ad6ae776d8f09c"    
     instance_type = "t2.nano"
@@ -68,6 +105,18 @@ resource "aws_instance" "name" {
     subnet_id = aws_subnet.prod.id
     vpc_security_group_ids = [aws_security_group.prod.id]
     tags = {
-        Name = "dev-terraform-custom"
+        Name = "EC2-terraform-public"
+        }  
+}
+
+#Create public instance
+resource "aws_instance" "name_pvt" {
+    ami = "ami-085ad6ae776d8f09c"    
+    instance_type = "t2.nano"
+    key_name = "key-n-virginia"
+    subnet_id = aws_subnet.prod_private.id
+    vpc_security_group_ids = [aws_security_group.prod.id]
+    tags = {
+        Name = "EC2-terraform-private"
         }  
 }
