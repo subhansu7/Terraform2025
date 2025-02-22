550165f8b8c76368fcdb31d3ad59e6f2e1065c94
subhansu7 <sarangisubhansu7@gmail.com>
2025-02-22 19:04:35 +0530

    Added Amazon AMI Id feature to existing code

--- Day-10-DataSource-Block/main.tf
+++ Day-10-DataSource-Block/main.tf
@@ -32,3 +32,27 @@ resource "aws_instance" "dev1" {
     Name = "EC2-terraform"
   }
 }
+
+/*
+#Below code to get the latest Amazon Linux AMI ID.
+  data "aws_ami" "amzlinux" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name = "name"
+    values = [ "amzn2-ami-hvm-*-gp2" ]
+  }
+  filter {
+    name = "root-device-type"
+    values = [ "ebs" ]
+  }
+  filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+  filter {
+    name = "architecture"
+    values = [ "x86_64" ]
+  }
+}
+*/
