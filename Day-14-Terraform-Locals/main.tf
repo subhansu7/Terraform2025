4015a6259646e9fbd1eae28b5b53a31a646c9b50
subhansu7 <sarangisubhansu7@gmail.com>
2025-02-27 18:27:02 +0530

    Example of Locals

--- Day-14-Terraform-Locals/main.tf
+++ Day-14-Terraform-Locals/main.tf
@@ -1,17 +1,35 @@
+# #Example-1
+# locals {
+#   bucket-name = "${var.layer}-${var.env}-bucket-hydnaresh"
+# }
+
+# resource "aws_s3_bucket" "demo" {
+#     # bucket = "web-dev-bucket"
+#     # bucket = "${var.layer}-${var.env}-bucket-hyd"
+#     bucket = local.bucket-name
+#     tags = {
+#         # Name = "${var.layer}-${var.env}-bucket-hyd"
+#         Name = local.bucket-name
+#         Environment = var.env
+#     }  
+# }
+
+#Example-2
+
 locals {
-  bucket-name = "${var.layer}-${var.env}-bucket-hydnaresh"
+  region = "us-east-1"
+  ami = "ami-0440d3b780d96b29d"
+  environment = "dev"
+  instance_type = "t2.micro"
+  Name = var.name
 }
 
-resource "aws_s3_bucket" "demo" {
-    # bucket = "web-dev-bucket"
-    # bucket = "${var.layer}-${var.env}-bucket-hyd"
-    bucket = local.bucket-name
-    tags = {
-        # Name = "${var.layer}-${var.env}-bucket-hyd"
-        Name = local.bucket-name
-        Environment = var.env
-    }
-    
- 
-  
+resource "aws_instance" "example" {
+  ami           = local.ami
+  instance_type = local.instance_type
+
+  tags = {
+    Name        = local.Name
+    Environment = local.environment
+  }
 }
--- Day-14-Terraform-Locals/variables.tf
+++ Day-14-Terraform-Locals/variables.tf
@@ -6,25 +6,11 @@ variable "layer" {
 
 variable "env" {
     type = string
-    default = "test"
+    default = "env-171"
   
 }
 
-
-#Example-2
-
-locals {
-  region = "us-east-1"
-  environment = "dev"
-  instance_type = "t2.micro"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
-  instance_type = local.instance_type
-
-  tags = {
-    Name        = "ExampleInstance"
-    Environment = local.environment
-  }
+variable "name" {
+type = string
+default = "dev"
 }
